4af8f22ecf5abe3666baf991d8c8653b925a5522
kakeiii <luckyta2002@gmail.com>
2026-09-21 14:28:03 -0400

    ajouter les tests historiques

--- src/test/java/ca/cegepmv/atelier/CalculatriceAvecHistoriqueTest.java
+++ src/test/java/ca/cegepmv/atelier/CalculatriceAvecHistoriqueTest.java
@@ -54,21 +54,23 @@ class CalculatriceAvecHistoriqueTest {
     void additionnerEnregistreUneEntreeDansLeJournal() {
         // TODO: Act
         // Appelez calculatrice.additionner(2, 3)
-
+        calculatrice.additionner(2, 3);
         // TODO: Assert
         // Utilisez verify(journalMock).enregistrer(...) pour vérifier qu'un message a bien
         // été enregistré. Astuce : vous pouvez vérifier le message exact attendu :
         // verify(journalMock).enregistrer("2 + 3 = 5");
-        fail("Test à compléter");
+        verify(journalMock).enregistrer("2 + 3 = 5");
+        
     }
 
     @Test
     void diviserParZeroNeFaitAucunEnregistrementDansLeJournal() {
         // TODO: Act + Assert
         // Vérifiez que diviser(10, 0) lance bien une ArithmeticException (comme avant),
+        assertThrows(ArithmeticException.class, () -> calculatrice.diviser(10,0));
         // ET utilisez verify(journalMock, never()).enregistrer(anyString()) pour vérifier
         // qu'aucun message n'a été enregistré dans ce cas (puisque l'exception est lancée
         // AVANT l'appel à journal.enregistrer(...)).
-        fail("Test à compléter");
+        verify(journalMock, never()).enregistrer(anyString());
     }
 }
